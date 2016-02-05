dd22c9ff8ca257eafaecb5c48fb189f5f53c3658
Kathryne Dunlap <kathryne@Kathrynes-MacBook-Pro.local>
2016-02-05 15:01:47 -0600

    11 Tests Passing Now

--- Money.rb
+++ Money.rb
@@ -1,17 +1,49 @@
-class USADollars
+class Currencies
 
 	def initialize(amt)
 		@amt = amt
 	end
 
+    def to_i
+    	@amt.to_i
+    end
+
 	def ==(other)
-		@amt == other
+		if abbreviation == other.abbreviation
+			@amt == other.to_i
+		elsif abbreviation == "$" && other.abbreviation == "CHF"
+		 	(@amt * 2) == other.to_i
+		elsif abbreviation == "CHF" && other.abbreviation == "$"
+			(@amt / 2) == other.to_i
+		end
 	end
 
+end
+
+class USADollars < Currencies
+
+	def abbreviation
+		"$"
+	end
 
 	def times(multiplier)
 		x = USADollars.new(@amt * multiplier)
-    end
+	end
 
 end
+
+class SwissFrancs < Currencies
+
+	def abbreviation
+		"CHF"
+	end
+
+	def times(multiplier)
+		x = SwissFrancs.new(@amt * multiplier)
+	end
+
+end
+
+
+
 
--- MoneyTest.rb
+++ MoneyTest.rb
@@ -5,7 +5,7 @@ class Money < Minitest::Test
 	def test_five_dollars_is_not_six_dollars
 		x = USADollars.new(5)
 		y = USADollars.new(6)
-		diff y, x
+		refute_equal y, x
 	end
 
 	def test_five_dollars_is_five_dollars
@@ -20,5 +20,64 @@ class Money < Minitest::Test
 		assert_equal x.times(2), y
 	end
 
+	def test_five_times_three_is_fifteen
+		x = USADollars.new(5).times(3)
+		y = USADollars.new(15)
+		assert_equal x, y
+	end
+
+	def test_five_francs_is_five_francs
+		x = SwissFrancs.new(5)
+		y = SwissFrancs.new(5)
+		assert_equal x, y
+	end
+
+	def test_five_times_two_is_ten_francs
+		x = SwissFrancs.new(5).times(2)
+		y = SwissFrancs.new(10)
+		assert_equal x, y
+	end
+
+	def test_four_francs_is_not_four_dollars
+		x = USADollars.new(4)
+		y = SwissFrancs.new(4)
+		refute_equal x, y
+	end
+
+	def test_dollars_are_currency
+		x = USADollars.new(3)
+		assert_kind_of Currencies, x
+	end
+
+	def test_francs_are_currency
+		x = SwissFrancs.new(3)
+		assert_kind_of Currencies, x
+	end
+
+	def test_ten_francs_is_five_dollars
+		x = USADollars.new(5)
+		y = SwissFrancs.new(10)
+		assert_equal x, y
+	end
+
+	def test_five_dollars_is_ten_francs
+		x = USADollars.new(5)
+		y = SwissFrancs.new(10)
+		assert_equal y, x
+	end
+
+
 end
+
+
+#money rounding?
+#hashCode (??)
+#equal null
+#equal object (??)
+#common times
+#compare francs with dollars (??)
+#refactor "times" method
+
+
+
 
